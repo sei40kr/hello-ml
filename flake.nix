041a03adf26bee6a8ccfec6afa5a83dcd5255562
Seong Yong-ju <sei40kr@gmail.com>
2025-01-18 23:53:47 +0900

    chore: add nix flakes & .envrc

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.python3Packages.tkinter ];
+        };
+      }
+    );
+}
